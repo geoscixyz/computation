7371842cab499a5aa993376d2aa829e7ec5ecdb5
Lindsey Heagy <lindseyheagy@gmail.com>
2016-07-25 13:58:09 -0700

    take kwargs in suppress nonlocal image warn

--- docs/conf.py
+++ docs/conf.py
@@ -307,7 +307,7 @@ intersphinx_mapping = {'https://docs.python.org/': None}
 import sphinx.environment
 from docutils.utils import get_source_line
 
-def _supress_nonlocal_image_warn(self, msg, node):
+def _supress_nonlocal_image_warn(self, msg, node, **kwargs):
     if not msg.startswith('nonlocal image URI found:'):
         self._warnfunc(msg, '%s:%s' % get_source_line(node))
 
