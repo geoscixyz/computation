d18bc999138202633c8f810545372c7e8a471db4
seogi_macbook <sgkang09@github.com>
2017-06-13 07:26:13 +0800

    Making figures for MT tutorials.

--- notebooks/MTtutorial/MTforward.py
+++ notebooks/MTtutorial/MTforward.py
@@ -3,24 +3,24 @@ import scipy.sparse as sp
 
 def simulateMT(mesh, sigma, frequency, rtype="app_res"):
     """
-       Compute apparent resistivity and phase at each frequency. 
+       Compute apparent resistivity and phase at each frequency.
        Return apparent resistivity and phase for rtype="app_res",
-       or impedance for rtype="impedance" 
+       or impedance for rtype="impedance"
     """
-    
+
     omega = 2*np.pi*frequency # Angular frequency (rad/s)
-    
-    # Grad 
+
+    # Grad
     mesh.setCellGradBC([['dirichlet', 'dirichlet']]) # Setup boundary conditions
     Grad = mesh.cellGrad # Gradient matrix
 
     # MfMu
     mu = np.ones(mesh.nC)*mu_0 # magnetic permeability values for all cells
-    Mmu = Utils.sdiag(mesh.aveCC2F * mu) 
+    Mmu = Utils.sdiag(mesh.aveCC2F * mu)
 
     # Mccsigma
     sigmahat = sigma  # quasi-static assumption
-    Msighat = Utils.sdiag(sigmahat) 
+    Msighat = Utils.sdiag(sigmahat)
 
     # Div
     Div = mesh.faceDiv # Divergence matrix
@@ -28,7 +28,7 @@ def simulateMT(mesh, sigma, frequency, rtype="app_res"):
     # Right Hand Side
     B = mesh.cellGradBC  # a matrix for boundary conditions
     Exbc = np.r_[0., 1.] # boundary values for Ex
-    
+
     # A-matrix
     A = sp.vstack([
         sp.hstack([Grad, 1j*omega*Mmu]), # Top row of A matrix
@@ -37,17 +37,17 @@ def simulateMT(mesh, sigma, frequency, rtype="app_res"):
 
     # Right-hand side
     rhs = np.r_[
-        -B*Exbc, 
+        -B*Exbc,
         np.zeros(mesh.nC)
-    ] 
-    
+    ]
+
     Ainv = Solver(A) # Factorize A matrix
     sol = Ainv*rhs   # Solve A^-1 rhs = sol
     Ex = sol[:mesh.nC] # Extract Ex from solution vector u
     Hy = sol[mesh.nC:mesh.nC+mesh.nN] # Extract Hy from solution vector u
 
     Zxy = - 1./Hy[-1] # Impedance at the surface
-    
+
     if rtype.lower() == "impedance":
         return Zxy
 
@@ -55,6 +55,6 @@ def simulateMT(mesh, sigma, frequency, rtype="app_res"):
         app_res = abs(Zxy)**2 / (mu_0*omega)
         app_phase = np.rad2deg(np.arctan(Zxy.imag / Zxy.real))
         return app_res, app_phase
-    
+
     else:
         raise Exception, "rtype must be 'impedance' or 'app_res', not {}".format(rtype.lower())
